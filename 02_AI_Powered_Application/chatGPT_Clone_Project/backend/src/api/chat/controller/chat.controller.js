e93d918589d95edb3c07c9ab12ed540604680149
Jafar610 <jafarmohamed955@gmail.com>
2026-09-04 00:30:37 +0300

    Saving data into database and getting recent conversations history with a validation from DB implemented

--- 02_AI_Powered_Application/chatGPT_Clone_Project/backend/src/api/chat/controller/chat.controller.js
+++ 02_AI_Powered_Application/chatGPT_Clone_Project/backend/src/api/chat/controller/chat.controller.js
@@ -1,20 +1,20 @@
-import { errorHandler } from "../../../middleware/error-handler.js";
 import { createConversationService } from "../services/chat.service.js";
 
-export async function createConversationController(req, res){
-   try {
-        const {question} = req.body;
-        const data = await createConversationService(question);
-        res.status(201).json({
-            status: true,
-            message: "create conversation api success",
-            result: data
-        })
-   } catch (error) {
-       throw error
-   }
+export async function createConversationController(req, res) {
+  try {
+    const { question } = req.body;
+
+    const result = await createConversationService(question);
+    res.status(201).json({
+      status: true,
+      message: "create conversation api success",
+      data: result,
+    });
+  } catch (error) {
+    throw error;
+  }
 }
 
-export function getConversationController(req, res){
-    res.send('Conversation get api');
+export function getConversationController(req, res) {
+  res.send("Conversation get api");
 }
--- 02_AI_Powered_Application/chatGPT_Clone_Project/backend/src/api/chat/services/chat.service.js
+++ 02_AI_Powered_Application/chatGPT_Clone_Project/backend/src/api/chat/services/chat.service.js
@@ -1,13 +1,40 @@
-export async function createConversationService(question){
-    try {
-        if(!question.trim()){
-            const error = new Error('Question is required');
-            error.status = 400;
-            throw error;
-        }
-    } catch (error) {
-        throw error;
+import db from "../../../../db/db.config.js";
+
+//select History
+const getRecentConversations = async (limit = 5) => {
+  const NormalizationLimit = Number.parseInt(limit, 20);
+  const safeLimit =
+    Number.isNaN(NormalizationLimit) || NormalizationLimit <= 0
+      ? 20
+      : NormalizationLimit;
+
+  const [rows] = await db.execute(
+    `SELECT id, role, content, created_at FROM conversations ORDER BY id DESC limit ${safeLimit}`,
+  );
+
+  return rows.reverse();
+};
+
+
+
+export async function createConversationService(question) {
+  try {
+    // validation
+    if (!question.trim()) {
+      const error = new Error("Question is required");
+      error.status = 400;
+      throw error;
     }
 
+    // save data
+    await db.execute("INSERT INTO conversations (content) VALUES (?)", [
+      question,
+    ]);
 
+
+    const rows = await getRecentConversations(5);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
 }
