004d83ceb261394b0eab35f78293fc1cc9ca36eb
Jafar610 <jafarmohamed955@gmail.com>
2026-09-07 14:57:46 +0300

    Initial folder structure for full chatgpt clone that contain login, user authentication using react and tailwind framework.

---
+++ 02_AI_Powered_Application/Full_ChatGPT_Clone/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <div className="flex h-screen bg-gray-800 text-white">
+          <h1>hello chatgpt</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
