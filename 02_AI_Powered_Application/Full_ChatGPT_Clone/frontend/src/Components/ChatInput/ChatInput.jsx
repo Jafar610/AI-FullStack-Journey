b1274fe7ddcbd6537e2cd48029e37ea9014c85e6
Jafar610 <jafarmohamed955@gmail.com>
2026-09-10 23:40:22 +0300

    Chat input message integration with backend implemented successfully. also the submit handler function is updated

--- 02_AI_Powered_Application/Full_ChatGPT_Clone/frontend/src/Components/ChatInput/ChatInput.jsx
+++ 02_AI_Powered_Application/Full_ChatGPT_Clone/frontend/src/Components/ChatInput/ChatInput.jsx
@@ -1,50 +1,69 @@
-import {useState} from 'react'
-import { Plus, Mic, ArrowUp } from 'lucide-react'
-function ChatInput({messages, setMessages}) {
-    const [input, setInput] = useState("");
+import { useState } from "react";
+import { Plus, Mic, ArrowUp } from "lucide-react";
+import axios from 'axios';
+function ChatInput({ setMessages }) {
+  const [input, setInput] = useState("");
+  const [isLoading, setIsLoading] = useState(false)
+  const submitHandler = async () => {
+    if (!input.trim()) return;
+    const question = input;
 
-    const submitHandler =()=>{
-       if (!input.trim()) return;
-
-    const userMessage = {
-      role: "user",
-      content: input,
-    };
-
-    const botMessage = {
-      role: "assistant",
-      content: "This is a fake response 🤖",
-    };
-
-    setMessages([...messages, userMessage, botMessage]);
     setInput("");
-    
-
+    try {
+        setIsLoading(true);
+        const result = await axios.post('http://localhost:3000/api/chat/conversations', {question})
+        
+        const res = result.data;
+        
+        if(res.success){
+            const {userConversation, assistantConversation} = res.data;
+            setMessages((prev)=>[
+                ...prev,
+                {
+                    role:userConversation.role,
+                    content:userConversation.content,
+                },
+                {
+                    role:assistantConversation.role,
+                    content:assistantConversation.content,
+                }
+            ])
+        }
+    } catch (error) {
+        throw error;
     }
+   finally{
+    setIsLoading(false);
+   }
+
+  };
 
   return (
     <>
-        <div className='p-4 bg-transparent'>
-            <div className='flex items-center rounded-full bg-gray-900 px-3 py-2 gap-2 mx-40'>
-
-                <button>
-                    <Plus size={20}/>
-                </button>
-                <input type="text" placeholder='Ask anything' className='flex-1 outline-none bg-transparent placeholder-gray-400' value={input}  onChange={(e) => setInput(e.target.value)} />
-                <button className='bg-blue-500 p-2 rounded-full hover:bg-blue-600' onClick={submitHandler}>
-                    
-                    {
-                        input.trim() === ''? (
-                            <Mic size={20}/>
-                        ):(
-                            <ArrowUp size={20}/>
-                        )
-                    }
-                </button >
-            </div>
+      <div className="p-4 bg-transparent">
+        <div className="flex items-center rounded-full bg-gray-900 px-3 py-2 gap-2 mx-40">
+          <button>
+            <Plus size={20} />
+          </button>
+          <input
+            type="text"
+            placeholder="Ask anything"
+            className="flex-1 outline-none bg-transparent placeholder-gray-400"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e)=>e.key === 'Enter' && submitHandler()}
+          />
+          <button
+            className="bg-blue-500 p-2 rounded-full hover:bg-blue-600"
+            onClick={submitHandler}
+            disabled={isLoading}
+          >
+            {input.trim() === "" ? <Mic size={20} /> : <ArrowUp size={20} />}
+          </button>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default ChatInput
+export default ChatInput;
