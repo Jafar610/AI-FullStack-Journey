be989e66b4bdd366ecc0c3a3fac82ab5c1ab8e2c
Jafar610 <jafarmohamed955@gmail.com>
2026-09-12 23:53:50 +0300

    Login frontend structure is fully implemented next i will integrate the css on it.

--- 02_AI_Powered_Application/chatGPT_Clone_Project/frontend/src/pages/Login.jsx
+++ 02_AI_Powered_Application/chatGPT_Clone_Project/frontend/src/pages/Login.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='main-container'>
+        <div className='content-wrapper'>
+            <h1>Create Your Account</h1>
+            <p>Note that phone verification may be required for signup. Your number will only be used to verify your identitfy for security puporses.</p>
+
+            <form action="">
+                <input type="text" name="" id="" /><br />
+                <input type="email" name="" id="" /><br />
+                <input type="password" name="" id="" /><br />
+                <button>Continue</button>
+            </form>
+
+            <p>Already have an account? <a href="">Log in</a></p>
+        </div>
+    </div>
   )
 }
 
