aa2758ed3289b65992bad56b9ebeffc13c197efd
Jafar610 <jafarmohamed955@gmail.com>
2026-09-14 22:11:48 +0300

    Routing to different page based on there token is implemented. so the system check the user token the it navigate to the chat page.

--- 02_AI_Powered_Application/chatGPT_Clone_Project/frontend/src/App.jsx
+++ 02_AI_Powered_Application/chatGPT_Clone_Project/frontend/src/App.jsx
@@ -1,13 +1,27 @@
+import ChatSharedLayout from "./Components/ChatSharedLayout/ChatSharedLayout";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 import { Routes, Route, Navigate } from "react-router-dom";
 function App() {
-  
+  const token = localStorage.getItem("token");
 
   return (
-    <>
-    
-    </>
+      <Routes>
+        {
+          !token ? (
+            <>
+             <Route path="/login" element = {<Login/>} />
+             <Route path="/register" element = {<Registration/>} />
+             <Route path="*" element = {<Login/>} />
+            </>
+          ):(
+            <>
+            <Route path="/chat/conversation" element ={<ChatSharedLayout/>} />
+            <Route path="*" element = {<Navigate to="/" />} />
+            </>
+          )
+        }
+      </Routes>
   );
 }
 
