ac67d23241318bca909db692069501cb5832a670
Jafar610 <jafarmohamed955@gmail.com>
2026-09-14 21:58:49 +0300

    Restructuring the file by creating a new sharedLayout folder for both chat and login or regstration

--- 02_AI_Powered_Application/chatGPT_Clone_Project/frontend/src/App.jsx
+++ 02_AI_Powered_Application/chatGPT_Clone_Project/frontend/src/App.jsx
@@ -1,83 +1,13 @@
-import ChatHeader from "./Components/ChatHeader/ChatHeader";
-import MessageList from "./Components/MessageList/MessageList";
-import Sidebar from "./Components/Sidebar/Sidebar";
-import { useEffect, useRef, useState } from "react";
-import axios from "axios";
-import ChatInput from "./Components/ChatInput/ChatInput";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
+import { Routes, Route, Navigate } from "react-router-dom";
 function App() {
-  const [conversations, setConversations] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const lastMessageRef = useRef(null);
-
   
-  async function fetchConversations() {
-    try {
-      const response = await axios.get(
-        "http://localhost:3000/api/chat/conversations",
-      );
-
-      setConversations(response.data.data);
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-
-  async function handleSubmit(question) {
-    if (!question.trim()) {
-      return;
-    }
-
-    const tempQuestion = {
-      id: Date.now(),
-      content: question.trim(),
-      role: "user",
-    };
-    
-    setConversations((prev) => [...prev, tempQuestion]);
-    try {
-      setIsLoading(true);
-      const { data } = await axios.post(
-        "http://localhost:3000/api/chat/conversations",
-        {
-          question: question.trim(),
-        },
-      );
-
-      setConversations((prev) => [...prev, data?.data?.assistantConversation]);
-    } catch (error) {
-       console.error(error.message);
-    } finally {
-      setIsLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    fetchConversations();
-  }, []);
-
-  useEffect(()=>{
-    lastMessageRef.current?.scrollIntoView({behavior:'smooth'});
-
-  },[conversations, lastMessageRef.current])
 
   return (
-    <div className="app">
-      {/* <Sidebar />
-      <main className="chat">
-        <ChatHeader />
-        <MessageList 
-         conversations={conversations}
-         isLoading={isLoading}
-         lastMessageRef={lastMessageRef}
-         />
-        <ChatInput handleSubmit={handleSubmit} />
-      </main> */}
-
-       {/* <Login/> */}
-       <Registration/>
-    </div>
+    <>
+    
+    </>
   );
 }
 
---
+++ 02_AI_Powered_Application/chatGPT_Clone_Project/frontend/src/Components/ChatSharedLayout/ChatSharedLayout.jsx
@@ -0,0 +1,80 @@
+import ChatHeader from "../ChatHeader/ChatHeader";
+import MessageList from "../MessageList/MessageList";
+import Sidebar from "../Sidebar/Sidebar";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import ChatInput from "../ChatInput/ChatInput";
+
+
+function ChatSharedLayout() {
+  const [conversations, setConversations] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const lastMessageRef = useRef(null);
+
+  async function fetchConversations() {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/chat/conversations",
+      );
+
+      setConversations(response.data.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async function handleSubmit(question) {
+    if (!question.trim()) {
+      return;
+    }
+
+    const tempQuestion = {
+      id: Date.now(),
+      content: question.trim(),
+      role: "user",
+    };
+
+    setConversations((prev) => [...prev, tempQuestion]);
+    try {
+      setIsLoading(true);
+      const { data } = await axios.post(
+        "http://localhost:3000/api/chat/conversations",
+        {
+          question: question.trim(),
+        },
+      );
+
+      setConversations((prev) => [...prev, data?.data?.assistantConversation]);
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchConversations();
+  }, []);
+
+  useEffect(() => {
+    lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [conversations, lastMessageRef.current]);
+  return (
+    <>
+      <div className="app">
+        <Sidebar />
+        <main className="chat">
+          <ChatHeader />
+          <MessageList
+            conversations={conversations}
+            isLoading={isLoading}
+            lastMessageRef={lastMessageRef}
+          />
+          <ChatInput handleSubmit={handleSubmit} />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default ChatSharedLayout;
