fe7e9da019d80d19c0fc77e6fefbc772c73bef0f
Jafar610 <jafarmohamed955@gmail.com>
2026-09-19 18:05:08 +0300

    embedding content method is added to the demo project. it used to embed the content into vector values.

--- 03_Beyond_Keywords/The_Mathimatics_of_meaning/EmbedingDemo/demo.js
+++ 03_Beyond_Keywords/The_Mathimatics_of_meaning/EmbedingDemo/demo.js
@@ -4,3 +4,14 @@ import { GoogleGenAI } from "@google/genai";
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
 });
+const response = await ai.models.embedContent({
+ model: 'text-embedding-001',
+ contents: [
+   'What is your name?',
+   'What is your favorite color?',
+ ],
+ config: {
+   outputDimensionality: 64,
+ },
+});
+console.log(response);
